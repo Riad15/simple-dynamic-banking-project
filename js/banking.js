315c312d51c35e1359b16875ff4009c4a0da851d
asif riad <asifriad15@gmail.com>
2022-02-11 02:09:30 +0600

    complate dynamic deposite,withdraw,blance

--- js/banking.js
+++ js/banking.js
@@ -1,11 +1,37 @@
+//--------------deposite money--------------------- 
 document.getElementById('deposite-btn').addEventListener('click', function () {
     const depositeInput = document.getElementById('deposite-amount');
     const addDepositeAmount = depositeInput.value;
     const depsiteTotal = document.getElementById('deposite-total');
     const privusDepositeTotal = depsiteTotal.innerText;
     const updateDepositeTotal = parseFloat(privusDepositeTotal) + parseFloat(addDepositeAmount);
-    console.log(updateDepositeTotal);
     depsiteTotal.innerText = updateDepositeTotal;
     depositeInput.value = '';
+    //------------total balance update ---------------------------
+    const totalBalance = document.getElementById('my-acount');
+    const lastBalance = totalBalance.innerText;
+    updateBalance = parseFloat(addDepositeAmount) + parseFloat(lastBalance);
+    totalBalance.innerText = updateBalance;
+
 
 })
+//-----------------------------withdraw money-------------------------
+document.getElementById('withdraw-btn').addEventListener('click', function () {
+    const withdrawInput = document.getElementById('Withdraw-amount');
+    const nextWithdraw = withdrawInput.value;
+    const totalwithdraw = document.getElementById('withdraw-total');
+    const lastWithdraw = totalwithdraw.innerText;
+    const updateWithdraw = parseFloat(nextWithdraw) + parseFloat(lastWithdraw);
+    totalwithdraw.innerText = updateWithdraw;
+    withdrawInput.value = '';
+
+    // update my acount 
+
+    const totalBalance = document.getElementById('my-acount');
+    const lastBalance = totalBalance.innerText;
+    updateBalance = parseFloat(lastBalance) - parseFloat(nextWithdraw);
+    totalBalance.innerText = updateBalance;
+
+})
+//------------------------balance dynamic----------------------
+
