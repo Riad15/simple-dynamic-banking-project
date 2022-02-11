b8de0ebfbe78e10f6e8fa8d7627a6966edcccd2c
asif riad <asifriad15@gmail.com>
2022-02-11 17:43:06 +0600

    update deposite and withdraw functionality

--- js/banking.js
+++ js/banking.js
@@ -1,12 +1,18 @@
 //--------------deposite money--------------------- 
+function inputFunction(inputId) {
+    const inputValue = document.getElementById(inputId);
+    const inputAmount = inputValue.value;
+    inputValue.value = '';
+    return inputAmount;
+}
 document.getElementById('deposite-btn').addEventListener('click', function () {
-    const depositeInput = document.getElementById('deposite-amount');
-    const addDepositeAmount = depositeInput.value;
+
+    const addDepositeAmount = inputFunction('deposite-amount');
     const depsiteTotal = document.getElementById('deposite-total');
     const privusDepositeTotal = depsiteTotal.innerText;
     const updateDepositeTotal = parseFloat(privusDepositeTotal) + parseFloat(addDepositeAmount);
     depsiteTotal.innerText = updateDepositeTotal;
-    depositeInput.value = '';
+
     //------------total balance update ---------------------------
     const totalBalance = document.getElementById('my-acount');
     const lastBalance = totalBalance.innerText;
@@ -17,13 +23,11 @@ document.getElementById('deposite-btn').addEventListener('click', function () {
 })
 //-----------------------------withdraw money-------------------------
 document.getElementById('withdraw-btn').addEventListener('click', function () {
-    const withdrawInput = document.getElementById('Withdraw-amount');
-    const nextWithdraw = withdrawInput.value;
+    const nextWithdraw = inputFunction('Withdraw-amount');
     const totalwithdraw = document.getElementById('withdraw-total');
     const lastWithdraw = totalwithdraw.innerText;
     const updateWithdraw = parseFloat(nextWithdraw) + parseFloat(lastWithdraw);
     totalwithdraw.innerText = updateWithdraw;
-    withdrawInput.value = '';
 
     // update my acount 
 
